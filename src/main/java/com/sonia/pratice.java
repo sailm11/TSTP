b40f9e170709cdf8f126d2c3cdd4e7c74fd09e38
Soniabansal99 <ranimanisha375@gmail.com>
2026-01-25 18:46:17 +0530

    My first commit

---
+++ src/main/java/com/sonia/pratice.java
@@ -0,0 +1,15 @@
+package com.sonia;
+
+import java.util.ArrayList;
+
+public class pratice {
+    static void main(String[]args){
+        ArrayList al = new ArrayList<>();
+      al.add(9999);
+      System.out.println(al.toString());
+      al.add(9997);
+      al.add(6668);
+      al.add(4567);
+      al.add(7845);
+    }
+}
